76f74788356709e88f41231c7e0b17c7c5dfe9ed
KarinaKarina6 <55412063+KarinaKarina6@users.noreply.github.com>
2020-01-18 02:32:18 +0300

    pars

--- ProjectParser.py
+++ ProjectParser.py
@@ -16,15 +16,24 @@ class Parser:
             soup = BeautifulSoup(r.text, 'html.parser')
             price = soup.findAll("article")
             for article in range(len(price)):
-                data = ((day(price[article].findAll("span", {"class": "post__time"})[0].text)).split(' '))[:3]
+                d=((day(price[article].findAll("span", {"class": "post__time"})[0].text)).split(' '))
+                date = d[:3]+[d[-1]]
                 T = price[article].findAll("a", {"class": "inline-list__item-link hub-link"})
                 for tag in range(len(T)):
                     t = T[tag].text
-                    self.l.append([t] + data)
+                    row = [t] + date
+                    self.l.append([row[0], row[3] + '-' + m_number(row[2]) + '-' + row[1], row[4]])
 
-    def parserandstorage(self,fromday,frommonth,fromyear,today,tomonth,toyear):
+
+    def parserandstorage(self,fromdate,todate):
         s = DataStorage()
         read = s.get_data()
+
+        unique = []
+        for i in read:
+            if i[1:] not in unique:
+                unique.append(i[1:])
+
         url = 'https://habr.com/ru/all/page'
         numbpages = 100
         self.l = []
@@ -34,21 +43,20 @@ class Parser:
                 soup = BeautifulSoup(r.text, 'html.parser')
                 price = soup.findAll("article")
                 for article in range(len(price)):
-                    date = ((day(price[article].findAll("span", {"class": "post__time"})[0].text)).split(' '))[:3]
-                    if controlpars(date, fromday, frommonth, fromyear, today, tomonth, toyear) == "break" or s.stoppars(read,date):
+                    d = ((day(price[article].findAll("span", {"class": "post__time"})[0].text)).split(' '))
+                    date = d[:3] + [d[-1]]
+                    article_date = date[2] + '-' + m_number(date[1]) + '-' + date[0]
+                    if article_date > todate or [article_date] + [d[-1]] in unique:
                         break
-                    elif controlpars(date, fromday, frommonth, fromyear, today, tomonth, toyear) == "StopIteration":
+                    elif fromdate > article_date:
                         raise StopIteration
                     else:
                         T = price[article].findAll("a", {"class": "inline-list__item-link hub-link"})
                         for tag in range(len(T)):
                             t = T[tag].text
-                            self.l.append([t] + date)
+                            self.l.append([t, article_date, date[-1]])
         except StopIteration:
             pass
         data = list(map(tuple, self.l))
         d = DataStorage()
         d.add_data(data)
-
-
-
